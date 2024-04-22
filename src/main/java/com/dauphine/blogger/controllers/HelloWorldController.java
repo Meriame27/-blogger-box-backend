2d824f0e747e6bb702ce83b71f72bb2cb9686038
Meriame27 <meriame.slimane@dauphine.eu>
2024-04-22 15:10:19 +0200

    commit-3

--- src/main/java/com/dauphine/blogger/controllers/HelloWorldController.java
+++ src/main/java/com/dauphine/blogger/controllers/HelloWorldController.java
@@ -1,4 +1,6 @@
 package com.dauphine.blogger.controllers;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,7 +18,14 @@ public class HelloWorldController {
     }
 
     @GetMapping("hello/{name}")
-    public String hello (@PathVariable String name){
+    @Operation(
+            summary = "Hello by name endpoint",
+            description = "Returns 'Hello {name}' by path variable"
+    )
+    public String hello (
+            @Parameter(description = "Name to greet")
+            @PathVariable String name
+    ){
         return "Hello " + name;
     }
 }
