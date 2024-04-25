e5507e22363f31bb8e6a20d206db212091752181
Meriame27 <meriame.slimane@dauphine.eu>
2024-04-25 16:01:22 +0200

    commit7

--- src/main/java/models/Category.java
+++ src/main/java/models/Category.java
@@ -1,17 +1,42 @@
 package models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 import java.util.UUID;
 
+@Entity
+@Table(name = "category")
 public class Category {
-    public Category(UUID uuid, String myFirstCategory) {
+    @Id
+    @Column(name = "id")
+    private UUID id;
+    @Column(name = "name")
+    private String name;
+    public Category(UUID uuid, String name) {
+        this.id = id;
+        this.name = name;
     }
     public Category(UUID uuid) {
     }
 
+    public Category() {
+
+    }
+
     public UUID getId() {
-        return null;
+        return id;
+    }
+    public void setId(UUID id){
+        this.id = id;
+    }
+    public String getName(){
+        return name;
     }
 
     public void setName(String newName) {
+        this.name = name;
     }
 }
